45f8aca40f4cc68381d2c17504a87c1ced542b31
Arthur Andreas Stach <a_stach@2021-42.wog.local>
2025-05-20 16:50:56 +0200

    Implementierung der Klasse Course entsprechend der Planung

--- Course.py
+++ Course.py
@@ -1,7 +1,45 @@
-"""Course Klasse"""
+"""
+Course Klasse
+"""
+
 class Course:
-    def __init__(self, name: str, grading_scheme: str) -> None:
+    def __init__(self, name: str, grading_scheme: str, student_names: list[str]) -> None:
+        """
+        Erstellen der Klasse
+
+        Args:
+            name: Name des Kurses
+            grading_scheme: "sek1" oder "sek2" zum Festlegen des Bewertungssystems
+            student_names: Liste mit den Schülernamen
+        """
         self.course_name = name
         if grading_scheme in ('sek1', 'sek2'):
             self.grading_scheme = grading_scheme
+        else:
+            raise Exception('invalid grading scheme. Choose "sek1" or "sek2"')
+        self.student_names = student_names
 
+    def set_name(self, name: str) -> None:
+        self.name = name
+
+    def set_grading_scheme(self, grading_scheme: str) -> None:
+        if grading_scheme in ('sek1', 'sek2'):
+            self.grading_scheme = grading_scheme
+        else:
+            raise Exception('invalid grading scheme. Choose "sek1" or "sek2"')
+
+    def set_student_names(self, studen_names: list[str]) -> None:
+        self.student_names = studen_names
+
+    def add_student(self, student_name: str) -> None:
+        self.student_names.append(student_name)
+
+    def remove_student(self, student_name: str) -> None:
+        self.student_names.remove(student_name)
+
+
+
+
+"""Test"""
+if __name__ == '__main__':
+    test_course = Course('LK11', 'sek', ['alice', 'bob'])
