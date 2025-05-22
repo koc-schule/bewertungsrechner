e1d5597c14cacdc50d0ce88edf5a067693291227
SuepCoder <laurinmartin1211@gmail.com>
2025-05-22 09:07:07 +0200

    csv_files erstellt

--- utils/parser.py
+++ utils/parser.py
@@ -1,3 +1,31 @@
 """
 Beschreibung
 """
+
+
+def results_csv_file_to_object(csv_name: str) -> None:
+    results = []
+    file = open(csv_name, "r")
+    file.read()
+    for i in range(0, len(file)):
+        row = file.readline(i)
+        row = remove_unimportant_data(row)
+        results.append({
+            row[:row.index(",")]: row[row.index(",") + 1:]
+        })
+    print(results)
+
+
+def remove_unimportant_data(row: str) -> str:
+    counter = 0
+    for i in range(len(row)):
+        if row[i] == ",":
+            counter += 1
+        if counter == 1:
+            start_index = i
+            counter += 1
+        elif counter == 5:
+            row = row[:start_index] + row[i:]
+            return row
+
+results_csv_file_to_object("quiz_13965756.csv")
