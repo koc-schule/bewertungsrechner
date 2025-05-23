45553494b92d535a90e2b3fd820983bcada67b96
simboldt <simon.boldt@gmx.net>
2025-05-23 20:36:50 +0200

    Codekriterien in gui.py angepasst

--- gui.py
+++ gui.py
@@ -1,8 +1,8 @@
 from PyQt6.QtWidgets import *
 from mainwindow import Ui_MainWindow
-from Exam import Exam
-from Course import Course
-from Result import Result
+from exam import Exam
+from course import Course
+from result import Result
 
 # test Course
 klasse = Course("klasse", "sek1", ["Schüler1", "Schüler2", "Schüler3"])
@@ -16,26 +16,57 @@ courselist = [klasse]
 examlist = [lk]
 
 
-def search_exam(name, list):
+def search_exam(name: str, list: list):
+    """
+        Durchsucht eine liste von Klausuren anhand eines Namen
+
+        Args:
+            name (str): Name der gesuchten Klausur
+            list (list): Zu durchsuchende Liste
+        Returns:
+            Exam: gesuchte Klausur
+    """
+
     for i in list:
         if i.exam_name == name:
             return (i)
 
 
-def search_course(name, list):
+def search_course(name: str, list: list):
+    """
+        Durchsucht eine liste von Kursen anhand eines Namen
+
+        Args:
+            name (str): Name des gesuchten Kurses
+            list (list): Zu durchsuchende Liste
+        Returns:
+            Course: gesuchter Kurs
+    """
+
     for i in list:
         if i.course_name == name:
             return (i)
 
 
-def confirm_input(): # Startet show_evaluation_table
+def confirm_input():
+    """
+        Startet show_evaluation_table
+    """
     course = search_course(ui.choose_course_textEdit.toPlainText(), courselist)
     exam = search_exam(ui.choose_exam_textEdit.toPlainText(), examlist)
 
     show_evaluation_table(course, exam)
 
 
-def show_evaluation_table(course, exam): # Erstellt die Tabelle zur Eingabe der Bewertung
+def show_evaluation_table(course: Course, exam: Exam):
+    """
+        Erstellt die Tabelle zur Eingabe der Bewertung
+
+        Args:
+            course (Course): ausgewählter Kurs
+            exam (Exam): ausgewählte Klausur/Test/LK
+    """
+
     # Spalten und Zeilen festlegen
     ui.evaluation_input_tableWidget.setRowCount(len(course.student_names) + 2)
     ui.evaluation_input_tableWidget.setColumnCount(len(exam.tasks) + 2)
@@ -58,7 +89,11 @@ def show_evaluation_table(course, exam): # Erstellt die Tabelle zur Eingabe der 
         ui.evaluation_input_tableWidget.setItem(1, i + 1, QTableWidgetItem(str(task_points)))
 
 
-def confirm_evaluation():  # Erstellt ein Objekt der Klasse Result aus den Eingaben
+def confirm_evaluation():
+    """
+        Erstellt ein Objekt der Klasse Result aus den Eingaben
+        """
+
     # Eingegebene Daten lesen
     course = search_course(ui.choose_course_textEdit.toPlainText(), courselist)
     exam = search_exam(ui.choose_exam_textEdit.toPlainText(), examlist)
