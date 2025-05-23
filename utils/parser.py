e43e893993f7e370544ce7c0cc9331510f60cc88
schop07 <schopalu07@gmail.com>
2025-05-23 12:33:05 +0200

    Lesen und Schreiben von csv-Dateien für Course-Klasse hinzugefügt

--- utils/parser.py
+++ utils/parser.py
@@ -1,3 +1,52 @@
+import csv
+import utils.csv
+from Course import Course
+import os
+
+
+def course_csv_file_to_object(name: str) -> Course:
+    """
+    Einlesen der .csv-Dateien für Klassen
+
+    Args:
+        name (str): Name des Kurses (ohne .csv)
+
+    Returns:
+        Course | bool: Course-Objekt oder False, wenn Objekt nicht angelegt werden kann
+    """
+
+    path = os.getcwd() + "/csv_files/" + name + ".csv"
+    all_lines = utils.csv.read_csv_lines(path)
+    return Course(all_lines[0][:-1], all_lines[1][:-1], all_lines[2].split(";"))
+
+
+def course_object_to_csv_file(course: Course) -> None:
+    """
+    Schreibt eine .csv-Datei für einen Course
+
+    Args:
+        course (Course): Einzulesendes Course-Objekt
+    """
+
+    content = str(course.course_name + "\n" + course.grading_scheme + "\n")
+    for student in course.student_names:
+        content = content + (student + ";")
+
+    content = content[:-1]
+
+    path = os.getcwd() + "/csv_files/course_" + course.course_name + ".csv"
+
+    """Format: course_name \n grading_scheme \n student_names (Schüler mit ';' getrennt)"""
+    utils.csv.write_csv(path, content)
+
+
+"""Test für Course-Dateien Lesen und Schreiben"""
+if __name__ == '__main__':
+    test_course = Course('LK11', 'sek2', ['Nachname1, Vorname1', 'Nachname2, Vorname2'])
+    course_object_to_csv_file(test_course)
+    test_course_2 = course_csv_file_to_object("course_LK11")
+
+
 """
 Aus einer .csv-Datei werden Ergebnisse herausgelesen und in einer list[dict] zurückgegeben
 """
