5ed6ad9a33ddae9897df92ef34e0c63ea3e4927b
schop07 <schopalu07@gmail.com>
2025-05-26 11:20:06 +0200

    Formatierung

--- utils/writer.py
+++ utils/writer.py
@@ -5,6 +5,7 @@ import utils.csv
 from course import Course
 from result import Result
 from exam import Exam
+
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
@@ -32,7 +33,7 @@ def course_object_to_csv_file(course: Course) -> None:
 
 def exam_object_to_csv_file(exam: Exam) -> None:
     """
-    Schreibt eine .csv-Datei für eine Exam
+    Schreibt eine .csv-Datei (Namensformat: exam_examname.csv) aus einem Exam-Objekt
 
     Args:
         exam (Exam): Einzulesendes Exam-Objekt
@@ -48,40 +49,39 @@ def exam_object_to_csv_file(exam: Exam) -> None:
     utils.csv.write_csv(path, content)
 
 
-def result_object_to_csv_file(result:Result)->None:
-
-   """ Schreibt ein Result- Objekt in eine .csv-Datei um 
+def result_object_to_csv_file(result: Result) -> None:
+    """ Schreibt ein Result-Objekt in eine .csv-Datei um
        
-       Argumente: 
-            result: Result
+       Args:
+            result (Result): Einzulesendes Result-Objekt
    """
 
-   """Auswählen der Datei"""
-   filename  = "./csv_files/" + str((result.get_exam()).exam_name) + ".csv"
-   file = open(filename,"w")
+    """Auswählen der Datei"""
+    filename = "./csv_files/" + str((result.get_exam()).exam_name) + ".csv"
+    file = open(filename, "w")
 
-   """Erstellen der ersten Zeile der csv_datei (Spaltennamen)"""
-   task_names_list = list(((result.get_exam()).tasks).keys())
-   task_names = ""
-   for key in task_names_list:
-       task_names = task_names + str(key) + ", "
-   column_name = "Name, Date, Percent, Points Earned, Points Possible, "  + task_names + "\n"
-   file.write(column_name)
+    """Erstellen der ersten Zeile der csv_datei (Spaltennamen)"""
+    task_names_list = list(((result.get_exam()).tasks).keys())
+    task_names = ""
+    for key in task_names_list:
+        task_names = task_names + str(key) + ", "
+    column_name = "Name, Date, Percent, Points Earned, Points Possible, " + task_names + "\n"
+    file.write(column_name)
 
-   """Eintragen der Ergebnisse für jeden Schüler"""
-   for student in result.results:
+    """Eintragen der Ergebnisse für jeden Schüler"""
+    for student in result.results:
 
-       """gleichbleibende Einträge in Variablen speichern """
-       student_name = student["name"] + ","
-       exam_date = str(result.get_date()) + ","
-       percentage_earned = str(student["percentage_earned"]) + ","
-       points_earned = str(student["points_earned"]) + "," 
-       max_points = str((result.get_exam()).max_points) + ","
-       studentdata = student_name + exam_date + percentage_earned + points_earned + max_points 
+        """gleichbleibende Einträge in Variablen speichern """
+        student_name = student["name"] + ","
+        exam_date = str(result.get_date()) + ","
+        percentage_earned = str(student["percentage_earned"]) + ","
+        points_earned = str(student["points_earned"]) + ","
+        max_points = str((result.get_exam()).max_points) + ","
+        studentdata = student_name + exam_date + percentage_earned + points_earned + max_points
 
-       """Speichern der Punktzahlen je Aufgabe für einen Schüler """
-       for task in list(((result.get_exam()).tasks).keys()):
-           studentdata = studentdata + str(student[task]) + ","
-       studentdata = studentdata + "\n"
+        """Speichern der Punktzahlen je Aufgabe für einen Schüler """
+        for task in list(((result.get_exam()).tasks).keys()):
+            studentdata = studentdata + str(student[task]) + ","
+        studentdata = studentdata + "\n"
 
-       file.write(studentdata)       
+        file.write(studentdata)
