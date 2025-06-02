0a69b097e7151aa375fba8f8d4bbb7fca5281019
Schizophrenic Satanist <115879778+nierenversagen@users.noreply.github.com>
2025-06-02 19:34:29 +0200

    fixed some bugggs
    tf unresolved issues github

--- printer/log.py
+++ printer/log.py
@@ -1,6 +1,7 @@
 from . import *
 
 IS_DEV = True
+CLEAR_OLD_LOGS = True
 current_log_file_path = ""
 
 def log(content: str) -> None:
@@ -10,26 +11,38 @@ def log(content: str) -> None:
         log.write(f"{content}\n")
 
 def reset() -> None:
-    if not IS_DEV:
-        clear()
+    if not IS_DEV or CLEAR_OLD_LOGS:
+        clear_logs()
         return
-    if not check():
+    
+    if not exists_directory_and_log():
         open("logs/events.log", "w").write("")
         return
+    
     log = open("logs/events.log", "r").read()
     counter = sum(1 for f in Path("logs").iterdir() if f.is_file())
     log_old = open(f"logs/old_event_{counter}.log", "w").write(log)
     date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
     log_new = open("logs/events.log", "w").write(f"Event Log Nr. {counter + 1} at time {date}\n")
 
-def check() -> bool:
-    try:
-        open("logs/events.log")
-    except FileNotFoundError as exception:
+def exists_directory_and_log() -> bool:
+    """
+    Prüft ob /logs Ordner und logs/events.log existiert. Legt /logs an, wenn nicht existiert
+
+    Returns:
+        bool: False wenn events.log nicht existiert, True wenn existiert
+    """
+    if not Path("logs").is_dir():
+        os.mkdir("logs")
+        return False
+    elif not Path("logs/events.log").is_file():
         return False
     else:
         return True
 
-def clear() -> None:
+def clear_logs() -> None:
+    """
+    Löscht alle bestehenden Dateien im /log Ordner
+    """
     if Path("logs").is_dir():
         [f.unlink() for f in Path('logs').iterdir() if f.is_file()]
